67e20ebc8bd8b6fd63970dd05138a02b79f320cf
kiddac <kiddac2015@gmail.com>
2024-10-30 19:17:20 +0000

    updated header, updated check_redirect download code, removedtimeout from epg file

--- XKlass/usr/lib/enigma2/python/Plugins/Extensions/XKlass/update.py
+++ XKlass/usr/lib/enigma2/python/Plugins/Extensions/XKlass/update.py
@@ -85,7 +85,11 @@ def get_time_utc(timestring, fdateparse):
         return 0
 
 
-hdr = {'User-Agent': str(cfg.useragent.value)}
+hdr = {
+    'User-Agent': str(cfg.useragent.value),
+    'Connection': 'keep-alive',
+    'Accept-Encoding': 'gzip, deflate'
+}
 
 
 class XKlass_Update:
@@ -116,20 +120,20 @@ class XKlass_Update:
             pass
 
     def check_redirect(self, url):
-        x = ""
         retries = Retry(total=3, backoff_factor=1)
         adapter = HTTPAdapter(max_retries=retries)
-        http = requests.Session()
-        http.mount("http://", adapter)
-        http.mount("https://", adapter)
-        try:
-            x = http.get(url, headers=hdr, timeout=30, verify=False, stream=True)
-            url = x.url
-            x.close()
-            return str(url)
-        except Exception as e:
-            print(e)
-            return str(url)
+
+        with requests.Session() as http:
+            http.mount("http://", adapter)
+            http.mount("https://", adapter)
+
+            try:
+                response = http.get(url, headers=hdr, timeout=30, verify=False, stream=True)
+                url = response.url
+                return str(url)
+            except Exception as e:
+                print(e)
+                return str(url)
 
     def process_json_file(self):
         try:
@@ -202,9 +206,9 @@ class XKlass_Update:
 
             if scheme == "https" and sslverify:
                 sniFactory = SNIFactory(domain)
-                downloadPage(url, epgxmlfile, sniFactory, timeout=120).addCallback(self.downloadComplete).addErrback(self.downloadFailed)
+                downloadPage(url, epgxmlfile, sniFactory).addCallback(self.downloadComplete).addErrback(self.downloadFailed)
             else:
-                downloadPage(url, epgxmlfile, timeout=120).addCallback(self.downloadComplete).addErrback(self.downloadFailed)
+                downloadPage(url, epgxmlfile).addCallback(self.downloadComplete).addErrback(self.downloadFailed)
 
         except Exception as e:
             print(e)
