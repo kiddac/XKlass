65f5cb8d2fc8d44d51b59b700e881145aee486dd
kiddac <kiddac2015@gmail.com>
2024-12-01 13:18:56 +0000

    1.25-20241127 live front display amends, auto updater amends for same domains

--- XKlass/usr/lib/enigma2/python/Plugins/Extensions/XKlass/update.py
+++ XKlass/usr/lib/enigma2/python/Plugins/Extensions/XKlass/update.py
@@ -171,9 +171,9 @@ class XKlass_Update:
                     epgxmlfile = os.path.join(epgfolder, "epg.xml")
                     epgjsonfile = os.path.join(epgfolder, "epg.json")
 
-                    exists = any(str(domain) == str(x[0]) for x in self.urllist)
-                    if exists:
+                    if xmltv in [x[1] for x in self.urllist]:
                         continue
+
                     self.urllist.append([domain, xmltv, epgxmlfile, epgjsonfile])
 
                     if not os.path.exists(epgfolder):
